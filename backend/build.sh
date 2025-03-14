8062bc9144a279389977c0adfc0ed7653fa2659a
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-15 01:25:55 +0530

    fix: bash.sh updated

--- backend/build.sh
+++ backend/build.sh
@@ -1,3 +1,7 @@
 set -o errexit
 
-pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput
+pip install -r requirements.txt && python manage.py migrate && python manage.py collectstatic --noinput 
+
+if [[ $CREATE_SUPERUSER == "True" ]]; then
+    python manage.py createsuperuser --no-input
+fi
