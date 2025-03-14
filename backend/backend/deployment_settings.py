61810092ccec72b8bda9afdfe292f734a990078a
Rabah-Muhammed <rabarabi31@gmail.com>
2025-03-14 21:34:55 +0530

    fix:settings.py updated

--- backend/backend/deployment_settings.py
+++ backend/backend/deployment_settings.py
@@ -38,7 +38,7 @@ STORAGES = {
 
 DATABASES = {
     'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL', 'sqlite:///db.sqlite3'),
+        default= os.environ['DATABASE_URL'], 
         conn_max_age=600
     )
 }
--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -33,7 +33,7 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = env('DEBUG')
+DEBUG = True
 
 AUTH_USER_MODEL = 'api.User'
 
